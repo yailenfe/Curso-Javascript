8e94374217e19453f9f130b2ced4ede09b6cdb72
yailen fajardo <yailenfajardoegues@gmail.com>
2020-10-12 18:15:36 -0300

    curso de javascrit

---
+++ valores.js
@@ -0,0 +1,12 @@
+40   
+
+"diego de granada"
+
+true
+false
+
+null
+
+undefined
+
+[1,2,3]
---
+++ variable.js
@@ -0,0 +1,14 @@
+var nombre = "oscar"
+ // declarar
+ var edad;
+ // inicializar
+ edad = 30
+
+ edad;
+
+ var  elemtos =["computadora","celular"]
+
+  var perosna ={
+      nombre : "diego",
+      edad:30
+  }
